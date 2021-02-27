336e84570698f66d072df4b9d78a69091496441f
BillB <github@billbennett.org>
2021-02-26 21:49:25 -0700

    better naming

--- get_handler.py
+++ get_handler.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from flask import Flask
 
-from zr6Lib import zr6, zr6Lib
+from zr6CommandLib import zr6Constants, zr6Lib
 
 sys.path.append('/home/admin/.local/usr/bin')
 
@@ -29,119 +29,119 @@ def zones_all_off():
 
 @app.route("/zr6/zones/1/on", methods=['GET'])
 def zone_1_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_01))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_01))
 
 
 @app.route("/zr6/zones/1/off", methods=['GET'])
 def zone_1_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_01))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_01))
 
 
 @app.route("/zr6/zones/2/on", methods=['GET'])
 def zone_2_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_02))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_02))
 
 
 @app.route("/zr6/zones/2/off", methods=['GET'])
 def zone_2_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_02))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_02))
 
 
 @app.route("/zr6/zones/3/on", methods=['GET'])
 def zone_3_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_03))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_03))
 
 
 @app.route("/zr6/zones/3/off", methods=['GET'])
 def zone_3_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_03))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_03))
 
 
 @app.route("/zr6/zones/4/on", methods=['GET'])
 def zone_4_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_04))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_04))
 
 
 @app.route("/zr6/zones/4/off", methods=['GET'])
 def zone_4_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_04))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_04))
 
 
 @app.route("/zr6/zones/5/on", methods=['GET'])
 def zone_4_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_05))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_05))
 
 
 @app.route("/zr6/zones/5/off", methods=['GET'])
 def zone_5_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_05))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_05))
 
 
 @app.route("/zr6/zones/6/on", methods=['GET'])
 def zone_6_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_06))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_06))
 
 
 @app.route("/zr6/zones/6/off", methods=['GET'])
 def zone_6_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_06))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_06))
 
 
 @app.route("/zr6/zones/7/on", methods=['GET'])
 def zone_7_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_07))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_07))
 
 
 @app.route("/zr6/zones/7/off", methods=['GET'])
 def zone_7_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_07))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_07))
 
 
 @app.route("/zr6/zones/8/on", methods=['GET'])
 def zone_8_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_08))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_08))
 
 
 @app.route("/zr6/zones/8/off", methods=['GET'])
 def zone_8_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_08))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_08))
 
 
 @app.route("/zr6/zones/9/on", methods=['GET'])
 def zone_9_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_09))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_09))
 
 
 @app.route("/zr6/zones/9/off", methods=['GET'])
 def zone_9_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_09))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_09))
 
 
 @app.route("/zr6/zones/10/on", methods=['GET'])
 def zone_10_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_10))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_10))
 
 
 @app.route("/zr6/zones/10/off", methods=['GET'])
 def zone_10_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_10))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_10))
 
 
 @app.route("/zr6/zones/11/on", methods=['GET'])
 def zone_11_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_11))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_11))
 
 
 @app.route("/zr6/zones/11/off", methods=['GET'])
 def zone_11_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_11))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_11))
 
 
 @app.route("/zr6/zones/11/on", methods=['GET'])
 def zone_11_on():
-    return content_return(zr6Lib.set_zone_on(zr6.ZONE_11))
+    return content_return(zr6Lib.set_zone_on(zr6Constants.ZONE_11))
 
 
 @app.route("/zr6/zones/12/off", methods=['GET'])
 def zone_12_off():
-    return content_return(zr6Lib.set_zone_off(zr6.ZONE_12))
+    return content_return(zr6Lib.set_zone_off(zr6Constants.ZONE_12))
--- zr6CommandLib/zr6Lib.py
+++ zr6CommandLib/zr6Lib.py
@@ -2,7 +2,7 @@
 
 import socket
 
-import zr6
+import zr6Constants
 
 BUFFER_SIZE = 1024
 
@@ -12,7 +12,7 @@ def send_command(command):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        s.connect((zr6.ZR6_IP, zr6.ZR6_PORT))
+        s.connect((zr6Constants.ZR6_IP, zr6Constants.ZR6_PORT))
 
         # Send data
         s.send(command.encode())
@@ -29,57 +29,57 @@ def send_command(command):
 
 
 def set_current_zone(zone_in):
-    response = send_command(zr6.ZONE_SET_ACTIVE.format(zone=zone_in))
-    if response == zr6.REQUEST_RESPONSE_SET_ACTIVE.format(zone=zone_in):
+    response = send_command(zr6Constants.ZONE_SET_ACTIVE.format(zone=zone_in))
+    if response == zr6Constants.REQUEST_RESPONSE_SET_ACTIVE.format(zone=zone_in):
         return True
     else:
         return False
 
 
 def get_current_zone():
-    return send_command(zr6.REQUEST_STATUS)
+    return send_command(zr6Constants.REQUEST_STATUS)
 
 
 def send_zone_command(zone_in, command_in):
-    message = zr6.ZONE_SPECIFIC_COMMAND.format(zone=zone_in, command=command_in)
+    message = zr6Constants.ZONE_SPECIFIC_COMMAND.format(zone=zone_in, command=command_in)
     response = send_command(message)
 
-    if response == zr6.ZONE_SPECIFIC_COMMAND_RETURN.format(zone=zone_in, command=command_in):
+    if response == zr6Constants.ZONE_SPECIFIC_COMMAND_RETURN.format(zone=zone_in, command=command_in):
         return True
     else:
         return False
 
 
 def set_zone_on(zone_in):
-    return send_zone_command(zone_in, zr6.COMMAND_SOURCE_SELECT_2)
+    return send_zone_command(zone_in, zr6Constants.COMMAND_SOURCE_SELECT_2)
 
 
 def set_zone_off(zone_in):
-    return send_zone_command(zone_in, zr6.COMMAND_OFF)
+    return send_zone_command(zone_in, zr6Constants.COMMAND_OFF)
 
 
 def set_whole_home_on():
-    message = zr6.REQUEST_GLOBAL_COMMAND.format(command=zr6.COMMAND_SOURCE_SELECT_2)
+    message = zr6Constants.REQUEST_GLOBAL_COMMAND.format(command=zr6Constants.COMMAND_SOURCE_SELECT_2)
     response = send_command(message)
 
-    if response == zr6.REQUEST_GLOBAL_COMMAND.format(command=zr6.COMMAND_SOURCE_SELECT_2):
+    if response == zr6Constants.REQUEST_GLOBAL_COMMAND.format(command=zr6Constants.COMMAND_SOURCE_SELECT_2):
         return True
     else:
         return False
 
 
 def set_whole_home_off():
-    message = zr6.REQUEST_GLOBAL_COMMAND.format(command=zr6.COMMAND_OFF)
+    message = zr6Constants.REQUEST_GLOBAL_COMMAND.format(command=zr6Constants.COMMAND_OFF)
     response = send_command(message)
 
-    if response == zr6.REQUEST_GLOBAL_COMMAND.format(command=zr6.COMMAND_OFF):
+    if response == zr6Constants.REQUEST_GLOBAL_COMMAND.format(command=zr6Constants.COMMAND_OFF):
         return True
     else:
         return False
 
 
 def main():
-    set_current_zone(zr6.ZONE_05)
+    set_current_zone(zr6Constants.ZONE_05)
     print(get_current_zone())
     # set_zone_on(zr6.ZONE_05)
     # print(get_current_zone())
